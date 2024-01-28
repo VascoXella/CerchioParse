53fe8cc2b56102a55e0106d35103dd86839d2339
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-28 11:24:17 +0100

    Aggiungere i file di progetto.

---
+++ CerchioParse/Cerchio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioParse
+{
+    internal class Cerchio
+    {
+        public float r { get; set; } 
+
+        public Cerchio() 
+        {
+        }
+        public Cerchio(float r)
+        {
+            this.r = r;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Cerchio R={0}", r);
+        }
+        public static Cerchio Parse(String s)
+        {           
+            float r = float.Parse(s);
+            return new Cerchio(r);
+        }
+
+
+    }
+}
---
+++ CerchioParse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c1 = new Cerchio(10);
+            Console.WriteLine(c1.ToString());
+
+            Cerchio c2 = Cerchio.Parse("3");
+            Console.WriteLine("{0}", c2.r);
+
+            Console.ReadLine();
+        }
+    }
+}
